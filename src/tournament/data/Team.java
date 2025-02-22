45a6852e15d64bbe7d742e86dcf75ff7c7115cf7
Surky <cruz.gcp@gmail.com>
2025-02-22 11:45:18 +0100

    Team tostring added

--- src/tournament/data/Team.java
+++ src/tournament/data/Team.java
@@ -1,15 +1,30 @@
 package tournament.data;
 
+import java.util.Arrays;
+
 public class Team {
     Player[] players;
+    String name;
 
-    public Team(Player[] players) {
+    public Team(Player[] players, String name) {
         this.players = players;
+        this.name = name;
     }
     public Player[] getPlayers() {
         return players;
     }
     public void setPlayers(Player[] players) {
         this.players = players;
+    }
+
+    @Override
+    public String toString() {
+        String teammates = "";
+        for(Player p:players) {
+            teammates += p + "\n";
+        }
+        return "Team" + name +
+                " - Members: " + players.length + "/5 " +
+                teammates;
     }
 }
