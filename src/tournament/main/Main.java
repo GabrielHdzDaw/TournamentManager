4b863806064619c81624ae3f4bdb2d89d9138f95
gabrielhdzdaw <gabrielhernandezdaw@gmail.com>
2025-02-22 10:53:33 +0100

    first commit

---
+++ src/tournament/main/Main.java
@@ -0,0 +1,17 @@
+package tournament.main;
+
+/*. View available tournaments ordered by name
+1. View players information ordered by ranking and name
+2. View teams information ordered by ranking
+3. Add a new player to a team
+4. Find an exact player by name
+5. Find a player
+6. Find a team
+7. Show all the matches ordered by tournament name
+8. Update the result of the matches pending
+9. Exit*/
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
